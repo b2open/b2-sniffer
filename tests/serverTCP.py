0ef324569530b2529922171f136577b573c1c6ee
Cleiton Bueno <cleitonrbueno@gmail.com>
2022-01-12 12:42:08 -0300

    Simple programs for testing in tests/

---
+++ tests/serverTCP.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3 -u
+
+import json
+import socket
+import sys
+import time
+import random
+
+HOST = '127.0.0.1'
+PORT = 33000
+
+print("Started Server: PORT {}".format(PORT))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+    
+        conn, addr = s.accept()
+    
+        with conn:
+            print('Connected by {}:{}'.format(addr[0], addr[1]))
+            n=-1
+            while True:
+                # Generate random numbers from 0 to 999
+                n =  random.randint(0,999)
+
+                # Create dict{}
+                payload = {"id": n}
+            
+                # Mount JSON             
+                data = json.dumps(payload)
+            
+                # Print JSON
+                print("PAYLOAD: {}".format(data), flush=True)
+                sys.stdout.flush()
+
+                # Send JSON via Socket
+                conn.sendall(bytes(data, encoding='utf-8'))
+            
+                # Sleep 5s
+                time.sleep(.5)
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print("Bye...")
